22af2e4c6a19656fef5d7ba48c98d16ef3947ac1
Anne Ogongo <anneogongo98@gmail.com>
2022-08-26 00:45:59 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit",(a) => {
+    a.preventDefault()
+    toDo(a.target.stuff.value)
+    form.reset()
+  })
 });
+
+function toDo(todo) {
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.addEventListener("click", out)
+  btn.innerHTML = " done! "
+  p.innerHTML = `${todo}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#tasks").appendChild(p)
+}
+
+function out(b) {
+  b.target.parentNode.remove()
+}
+
+
+
